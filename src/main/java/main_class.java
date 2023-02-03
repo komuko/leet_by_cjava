0cc30f3d32ddfadf01c2e4377eed79327a06d670
komuko <komuko@163.com>
2023-02-03 17:11:06 +0800

    提交工程文件

---
+++ src/main/java/main_class.java
@@ -0,0 +1,6 @@
+public class main_class {
+    public static void main(String[] args){
+        test_java_class tjc = new test_java_class();
+        tjc.print_msg();
+    }
+}
---
+++ src/main/java/test_java_class.java
@@ -0,0 +1,6 @@
+public class test_java_class {
+    int count;
+    void print_msg(){
+        System.out.println("这是一条测试信息");
+    }
+}
